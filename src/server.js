836449b309c2efd3e1fb96265c052c69b504e300
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-10-13 13:58:39 -0300

    Base RestAPI in Node.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@databasefordevelopment-fd2hu.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(9000);
